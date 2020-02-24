5e8054a6d3ce8cc0d06a5933c859af7b3128e875
unknown <joydeep127@gmail.com>
2020-02-24 15:29:51 +0530

    report ui done

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import React, {
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import Landing from './components/landing/Landing'
-import Header from './components/landing/Navbar'
+import MyNavbar from './components/landing/Navbar'
 import Mortgage from './components/mortgage/Mortgage'
 import Preview from './components/mortgage/Preview'
 import PaymentLoan from './components/payments/PaymentLoan'
 import PaymentScheduler from './components/payments/PaymentScheduler'
 import { history } from "../src/components/helpers/history"
+import Config from './components/Config/Config'
+
+
 const App = () => {
   return (
 
     <Router history={history}>
       <Fragment>
         <Switch>
-          <Route exact path='/' render={() => <Header > <Landing /></Header>} />
-          <Route exact path='/mortgage' render={() => <Header > <Mortgage /></Header>} />
-          <Route exact path='/Preview' render={() => <Header > <Preview /></Header>} />
-          <Route exact path='/paymentLoan' render={() => <Header > <PaymentLoan /></Header>} />
-          <Route exact path='/paymentScheduler' render={() => <Header > <PaymentScheduler /></Header>} />
+          <Route exact path='/' render={() => <MyNavbar > <Landing /></MyNavbar>} />
+          <Route exact path='/mortgage' render={() => <MyNavbar > <Mortgage /></MyNavbar>} />
+          <Route exact path='/Preview' render={() => <MyNavbar > <Preview /></MyNavbar>} />
+          <Route exact path='/paymentLoan' render={() => <MyNavbar > <PaymentLoan /></MyNavbar>} />
+          <Route exact path='/paymentScheduler' render={() => <MyNavbar > <PaymentScheduler /></MyNavbar>} />
+          <Route exact path='/config' render={() => <MyNavbar > <Config /></MyNavbar>} />
         </Switch>
       </Fragment>
     </Router>
--- src/components/landing/Navbar.js
+++ src/components/landing/Navbar.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Navbar, Form, Button, Nav, FormControl } from 'react-bootstrap'
-import { Icon } from 'semantic-ui-react'
+import { Navbar, Form, Button, Nav, Row, Col, FormControl } from 'react-bootstrap'
+import { Header, Image, Modal } from 'semantic-ui-react'
+import { Button as MyBtn } from 'semantic-ui-react'
+import { Icon,Dropdown } from 'semantic-ui-react'
 import axios from 'axios';
+import './style.css'
 
-
-export default class Header extends React.Component {
+export default class MyNavbar extends React.Component {
     constructor() {
         super()
         this.state = {
             search: '',
+            showModal: false,
             user: {}
         }
+    }
+
+    onCloseClick = () => {
+        console.log("Close clicked");
+        this.setState({
+            showModal : false
+        })
+    }
+    showModal = () => {
+        console.log("Close clicked");
+        this.setState({
+            showModal : true
+        })
     }
 
     // async fetchKey() {
@@ -41,6 +57,11 @@ export default class Header extends React.Component {
     //     localStorage.setItem('reqID', val)
     // }
     render() {
+        const stateOptions = [
+            { key: "1", text: "Paid Emi", value: 1},
+            { key: "2", text: "UnPaid Emi", value: 2},
+            { key: "3", text: "All", value: 3},
+        ]
         return (
             <div>
                 <Navbar bg="primary" variant="dark" expand="lg">
@@ -52,6 +73,12 @@ export default class Header extends React.Component {
                         </Nav.Item>
                         <Nav.Item>
                             <Nav.Link eventKey="link-1" href='/paymentScheduler'>Payment Scheduler</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link onClick={this.showModal}>Reports</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href='/config'>Config</Nav.Link>
                         </Nav.Item>
                     </Nav>
                     <Navbar.Collapse id="basic-navbar-nav" className="left-spacing"  >
@@ -79,11 +106,92 @@ export default class Header extends React.Component {
 
                         <Form inline className="loginBtn">
                             <Button className="mr-sm-2" variant="outline-light" >LogIn </Button>
-
                         </Form>
                     </Navbar.Collapse>
                 </Navbar>
                 {this.props.children}
+                {/* <div class="reportModal"> */}
+                    <Modal open={this.state.showModal} style={{height: 400,minHeight: '380px',width:'70%', height: '50%',
+    marginLeft: '15%', marginTop:'5%'}} closeIcon onClose={this.onCloseClick}>
+                        <Modal.Header style={{textAlign: 'center'}}>Generate Report</Modal.Header>
+                        <Modal.Content>
+                            <Modal.Description>
+                            {/* <Header>Default Profile Image</Header> */}
+                            <Row >
+                            <Col className="same-row">
+                                <div className="reportCol">
+                                    <div className="name-wd" >
+                                        Request No. <sup style={{ color: 'red' }}>*</sup>:
+                                    </div >
+                                    <div className="ui input">
+                                        <input type="text" name="lname" placeholder="Request No." required />
+                                        {/* <input type="text"
+                                        style={{ borderColor: this.state.errorBorder ? this.state.errorBorder : '' }}
+                                        name="fname" onChange={(e) => this.handleOnChange(e)}
+                                        placeholder="firstName"
+                                        defaultValue={this.state.user.fname && this.state.user.fname} required /> */}
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col className="same-row">
+                                <div className="reportCol">
+                                    <div className="name-wd">
+                                        Start date<sup style={{ color: 'red' }}>*</sup>:
+                                    </div >
+                                    <div className="ui input">
+                                        <input type="date" name="Date" placeholder="Start Date" />
+                                    </div>
+                                </div>
+                                
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col className="same-row">
+                                <div className="reportCol">
+                                    <div className="name-wd">
+                                        End date<sup style={{ color: 'red' }}>*</sup>:
+                                    </div >
+                                    <div className="ui input">
+                                        <input type="date" name="Date" placeholder="End Date" />
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col className="same-row">
+                                <div className="reportCol">
+                                    <div className="name-wd">
+                                    Type<sup style={{ color: 'red' }}>*</sup>:
+                                    </div >
+                                    <div className="ui input">
+                                        <Dropdown placeholder='Report Type' search selection options={stateOptions} />
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                        {/* <Row >
+                            <Col className="same-row">
+                                <div className="name-space">
+                                    <div className="name-wd">
+                                    </div >
+                                    <div className="ui input">
+                                        <Button style={{ width: '150px', color: "white", backgroundColor: 'green' }}>Generate Report</Button>
+                                    </div>
+                                </div>
+                                
+                            </Col>
+                        </Row> */}
+                        <Row >
+                            <Button style={{ width: '130px', color: "white", backgroundColor: 'green', marginTop:"35px",marginLeft:'auto', marginRight:'auto',padding: '8px 10px' }}>Generate Report</Button>
+                        </Row>
+                            </Modal.Description>
+                        </Modal.Content>
+                    </Modal>
+                {/* </div> */}
+                
+                
             </div>
         )
     }
